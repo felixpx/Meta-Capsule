db617e22802910c52e77ee5cb59f23555564c76f
Felix <dexdaprotocol@gmail.com>
2022-03-08 18:51:04 -0500

    -account page

--- src/components/TopEnd/Navbar.js
+++ src/components/TopEnd/Navbar.js
@@ -12,12 +12,12 @@ export default function Navbar() {
   function openComission() {
     router.push('/matchmaker')
   }
-  function openAccount() {
-    router.push('/account')
-  }
+  // function openAccount() {
+  //   router.push('/account')
+  // }
 
   function openUpload() {
-    router.push('/upload')
+    router.push('/dashboard')
   }
 
   return (
@@ -27,7 +27,7 @@ export default function Navbar() {
         <button onClick={openDiscover}>Discover</button>
         {/* <button onClick={openShop}>Shop</button> */}
         <button onClick={openComission}>Matchmaker</button>
-        <button onClick={openAccount}>Account</button>
+        {/* <button onClick={openAccount}>Account</button> */}
         <button onClick={openUpload}>Upload Items</button>
       </div>
       <div className="top-18 absolute right-0 h-10 w-1/12 bg-gradient-to-r from-gray-800 lg:from-transparent lg:to-transparent" />
