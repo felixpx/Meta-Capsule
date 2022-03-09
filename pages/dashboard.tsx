4017a15aec9854a4b82731b9a02e75dcc6ab180f
Felix <dexdaprotocol@gmail.com>
2022-03-08 19:05:52 -0500

    dashboard update

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,69 +1,32 @@
-import { ClipboardCopyIcon, SunIcon, UserIcon } from '@heroicons/react/outline'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { useChain, useMoralis } from 'react-moralis'
+import { useMoralis } from 'react-moralis'
 import Header from '../src/components/TopEnd/Header'
 import Navbar from '../src/components/TopEnd/Navbar'
 import Login from '../src/components/Main/Account/Login'
 import MintItem from '../src/components/Main/Upload/MintItem'
+import ProfileInfo from '../src/components/Main/Account/ProfileInfo'
 
 const Home: NextPage = () => {
-  const router = useRouter()
-  const { switchNetwork } = useChain()
+  const { isAuthenticated, user } = useMoralis()
 
-  const {
-    isAuthenticated,
-    user,
-    chainId,
-    setUserData,
-    isUserUpdating,
-    isWeb3Enabled,
-    enableWeb3,
-  } = useMoralis()
-
-  const [walletAddress, setWalletAddress] = useState()
-  const [username, setUsername] = useState()
-  const [email, setEmail] = useState()
-  const [website, setWebsite] = useState()
-  const [wrongNetwork, setWrongNetwork] = useState(Boolean)
-  const [changeUser, setChangeUser] = useState(false)
+  const [noUser, setNoUser] = useState(Boolean)
+  const [upload, setUpload] = useState(Boolean)
 
   useEffect(() => {
-    if (isWeb3Enabled) enableWeb3()
-    if (isAuthenticated && chainId != null) {
-      if (user) {
-        setUsername(user.get('username'))
-        setWalletAddress(user.get('ethAddress'))
-        setWrongNetwork(false)
-      }
-    } else if (isAuthenticated && chainId != '0x13881') {
-      if (user) {
-        setUsername(user.get('username'))
-        setWalletAddress(user.get('ethAddress'))
-      }
-      setWrongNetwork(true)
-    } else {
+    if (isAuthenticated && user) {
+      setNoUser(false)
+    } else if (!isAuthenticated) {
+      setNoUser(true)
     }
-  }, [user, isAuthenticated, chainId])
-
-  const setUser = async () => {
-    const username = (document.getElementById('username') as HTMLInputElement)
-      .value
-    const email = (document.getElementById('email') as HTMLInputElement).value
-    const website = (document.getElementById('website') as HTMLInputElement)
-      .value
-    await setUserData({
-      username: username,
-      email: email,
-      website: website,
-    })
-    setChangeUser(false)
-  }
+  }, [isAuthenticated, user])
 
   if (!isAuthenticated) return <Login />
+
+  function uploading() {
+    setUpload(true)
+  }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center pb-2">
@@ -75,6 +38,12 @@ const Home: NextPage = () => {
         <Header />
         <Navbar />
       </header>
+      {!noUser && (
+        <div className="fixed top-36 right-12 z-50 w-64 rounded-xl bg-white bg-opacity-30 p-4">
+          <ProfileInfo />
+        </div>
+      )}
+
       <main className="relative flex h-screen w-full flex-col items-center justify-center text-center">
         <div className="flex w-full flex-row items-center justify-evenly">
           <div className="absolute top-16 flex flex-col items-start justify-center sm:w-9/12">
@@ -84,11 +53,11 @@ const Home: NextPage = () => {
                 className="whitespace-nowrap rounded-lg border-2 border-gray-800 px-2"
                 // onClick={}
               >
-                Matches
+                Collection
               </button>
               <button
                 className="whitespace-nowrap rounded-lg border-2 border-gray-800 px-2"
-                // onClick={}
+                onClick={uploading}
               >
                 Upload Items
               </button>
@@ -96,24 +65,26 @@ const Home: NextPage = () => {
                 className="whitespace-nowrap rounded-lg border-2 border-gray-800 px-2"
                 // onClick={}
               >
-                Collection
+                Matches
               </button>
             </div>
           </div>
-          <div className="absolute top-36 flex w-full items-center justify-center sm:top-0 sm:justify-end sm:opacity-0">
+          {/* <div className="absolute top-36 flex w-full items-center justify-center sm:top-0 sm:justify-end sm:opacity-0">
             <SunIcon className="h-12" />
-          </div>
+          </div> */}
         </div>
         <div className="absolute top-44 w-full">
-          <div className="mb-8 flex w-full flex-col items-center justify-center">
-            <div className="flex w-full flex-row items-center justify-evenly">
-              <p className="mb-8 text-2xl">Mint Collection</p>
+          {upload && (
+            <div className="flex w-full flex-col items-center justify-center">
+              <div className="flex w-full flex-row items-center justify-evenly">
+                <p className="mb-4 text-2xl">Mint Item</p>
+              </div>
+              <MintItem />
             </div>
-            <MintItem />
-          </div>
+          )}
         </div>
       </main>
-      <footer className="flex h-24 w-full items-center justify-center border-t">
+      {/* <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -123,7 +94,7 @@ const Home: NextPage = () => {
           Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
